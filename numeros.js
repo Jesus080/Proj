b1a09373cfa157895978498fdf1ac17318f4785d
Jesus080 <jg170184@gmail.com>
2025-03-13 16:24:29 -0600

    automa

---
+++ numeros.js
@@ -0,0 +1,19 @@
+function encontrarMaximo(arr) {
+    if (arr.length === 0) {
+        return "El arreglo está vacío";
+    }
+
+    let maximo = arr[0]; // Suponemos que el primer elemento es el mayor
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+    }
+
+    return maximo;
+}
+
+// Ejemplo de uso
+let numeros = [12, 45, 67, 89, 23, 90, 34];
+console.log("El número más grande es:", encontrarMaximo(numeros));
